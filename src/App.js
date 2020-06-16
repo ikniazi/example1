aee0c260bf2c97402240ecd47851d93d695de271
ikniazi <45164279+ikniazi@users.noreply.github.com>
2020-06-16 11:54:02 +0500

    update courtName

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App()
   return(
 
   <div className="App">
-    <Dinner courtName=" Disttict and Session Court"></Dinner>
+    <Dinner courtName=" Disttict and Session Courts"></Dinner>
   </div> 
   );
 }
